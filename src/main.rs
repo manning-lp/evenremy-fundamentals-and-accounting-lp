dd437af37fc53d3a96007d95ce404f0deb4a76ef
Remy EVEN <github@reven.fr>
2025-12-15 07:02:20 +0100

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::AccountingError::AccountUnderFunded;
 use std::collections::HashMap;
+use std::io::{stdin, Write};
 
 /// An application-specific error type
 #[derive(Debug)]
@@ -115,78 +116,95 @@ impl Accounts {
     }
 }
 
+fn read_from_stdin(label: &str) -> String {
+    print!("{}", label);
+    std::io::stdout().flush().unwrap_or_default();
+    let mut line = String::new();
+    stdin().read_line(&mut line).unwrap_or_else(|e| {
+        line = "".to_string();
+        return 0
+    });
+
+    line.trim().to_string()
+}
+
 fn main() {
-    println!("Hello, accounting world!");
-
-    // We are using simple &str instances as keys
-    // for more sophisticated keys (e.g. hashes)
-    // the data type could remain the same
-    let bob = "bob";
-    let alice = "alice";
-    let charlie = "charlie";
-    let initial_amount = 100;
-
-    // Creates the basic ledger and a tx log container
-    let mut ledger = Accounts::new();
-    let mut tx_log = vec![];
-
-    // Deposit an amount to each account
-    for signer in [bob, alice, charlie] {
-        let status = ledger.deposit(signer, initial_amount);
-        println!("Depositing {} for {}: {:?}", signer, initial_amount, status);
-        // Add the resulting transaction to a list of transactions
-        // .unwrap() will crash the program if the status is an error.
-        tx_log.push(status.unwrap());
+    loop {
+        let cmd = read_from_stdin("cmd:");
+        println!("{}",cmd)
     }
 
-    // Send currency from one account (bob) to the other (alice)
-    let send_amount = 10_u64;
-    let status = ledger.send(bob, alice, send_amount);
-    println!(
-        "Sent {} from {} to {}: {:?}",
-        send_amount, bob, alice, status
-    );
-
-    // Add both transactions to the transaction log
-    let (tx1, tx2) = status.unwrap();
-    tx_log.push(tx1);
-    tx_log.push(tx2);
-
-    // Withdraw everything from the accounts
-    let tx = ledger.withdraw(charlie, initial_amount).unwrap();
-    tx_log.push(tx);
-    let tx = ledger
-        .withdraw(alice, initial_amount + send_amount)
-        .unwrap();
-    tx_log.push(tx);
-
-    // Here we are withdrawing too much and there won't be a transaction
-    println!(
-        "Withdrawing {} from {}: {:?}",
-        initial_amount,
-        bob,
-        ledger.withdraw(bob, initial_amount)
-    );
-    // Withdrawing the expected amount results in a transaction
-    let tx = ledger.withdraw(bob, initial_amount - send_amount).unwrap();
-    tx_log.push(tx);
-
-    // {:?} prints the Debug implementation, {:#?} pretty-prints it
-    println!("Ledger empty: {:?}", ledger);
-    println!("The TX log: {:#?}", tx_log);
-
-    // add max u64 to alice and bob
-    let tx = ledger.deposit(alice, u64::MAX).unwrap();
-    tx_log.push(tx);
-    let tx = ledger.deposit(bob, u64::MAX).unwrap();
-    tx_log.push(tx);
-    println!("Ledger maxed: {:?}", ledger);
-    let expected_amount = *ledger.accounts.get(alice).unwrap();
-    let e = ledger.send(alice, bob, 1000).unwrap_err();
-    println!("send error: {:?}", e);
-    println!(
-        "alice refunded: {}, bob untouched: {}",
-        *ledger.accounts.get(alice).unwrap() == expected_amount,
-        *ledger.accounts.get(bob).unwrap() == expected_amount
-    );
+    // println!("Hello, accounting world!");
+    //
+    // // We are using simple &str instances as keys
+    // // for more sophisticated keys (e.g. hashes)
+    // // the data type could remain the same
+    // let bob = "bob";
+    // let alice = "alice";
+    // let charlie = "charlie";
+    // let initial_amount = 100;
+    //
+    // // Creates the basic ledger and a tx log container
+    // let mut ledger = Accounts::new();
+    // let mut tx_log = vec![];
+    //
+    // // Deposit an amount to each account
+    // for signer in [bob, alice, charlie] {
+    //     let status = ledger.deposit(signer, initial_amount);
+    //     println!("Depositing {} for {}: {:?}", signer, initial_amount, status);
+    //     // Add the resulting transaction to a list of transactions
+    //     // .unwrap() will crash the program if the status is an error.
+    //     tx_log.push(status.unwrap());
+    // }
+    //
+    // // Send currency from one account (bob) to the other (alice)
+    // let send_amount = 10_u64;
+    // let status = ledger.send(bob, alice, send_amount);
+    // println!(
+    //     "Sent {} from {} to {}: {:?}",
+    //     send_amount, bob, alice, status
+    // );
+    //
+    // // Add both transactions to the transaction log
+    // let (tx1, tx2) = status.unwrap();
+    // tx_log.push(tx1);
+    // tx_log.push(tx2);
+    //
+    // // Withdraw everything from the accounts
+    // let tx = ledger.withdraw(charlie, initial_amount).unwrap();
+    // tx_log.push(tx);
+    // let tx = ledger
+    //     .withdraw(alice, initial_amount + send_amount)
+    //     .unwrap();
+    // tx_log.push(tx);
+    //
+    // // Here we are withdrawing too much and there won't be a transaction
+    // println!(
+    //     "Withdrawing {} from {}: {:?}",
+    //     initial_amount,
+    //     bob,
+    //     ledger.withdraw(bob, initial_amount)
+    // );
+    // // Withdrawing the expected amount results in a transaction
+    // let tx = ledger.withdraw(bob, initial_amount - send_amount).unwrap();
+    // tx_log.push(tx);
+    //
+    // // {:?} prints the Debug implementation, {:#?} pretty-prints it
+    // println!("Ledger empty: {:?}", ledger);
+    // println!("The TX log: {:#?}", tx_log);
+    //
+    // // add max u64 to alice and bob
+    // let tx = ledger.deposit(alice, u64::MAX).unwrap();
+    // tx_log.push(tx);
+    // let tx = ledger.deposit(bob, u64::MAX).unwrap();
+    // tx_log.push(tx);
+    // println!("Ledger maxed: {:?}", ledger);
+    // let expected_amount = *ledger.accounts.get(alice).unwrap();
+    // let e = ledger.send(alice, bob, 1000).unwrap_err();
+    // println!("send error: {:?}", e);
+    // println!(
+    //     "alice refunded: {}, bob untouched: {}",
+    //     *ledger.accounts.get(alice).unwrap() == expected_amount,
+    //     *ledger.accounts.get(bob).unwrap() == expected_amount
+    // );
 }
