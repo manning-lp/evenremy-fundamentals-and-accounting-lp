4c1ea88d2fecf21538bd412a0220ec0c8e517421
Remy EVEN <github@reven.fr>
2025-12-16 14:03:55 +0100

    modules accounts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::io::{stdin, Write};
 
 mod error {
@@ -21,103 +20,109 @@ mod tx {
     }
 }
 
-/// A type for managing accounts and their current currency balance
-#[derive(Debug)]
-struct Accounts {
-    accounts: HashMap<String, u64>, // id to amount
-}
-
-impl Accounts {
-    /// Returns an empty instance of the [`Accounts`] type
-    pub fn new() -> Self {
-        Accounts {
-            accounts: Default::default(),
-        }
+mod accounts {
+    use crate::{error, tx};
+    use std::collections::HashMap;
+    /// A type for managing accounts and their current currency balance
+    #[derive(Debug)]
+    pub(crate) struct Accounts {
+        accounts: HashMap<String, u64>, // id to amount
     }
 
-    /// Either deposits the `amount` provided into the `signer` account or adds the amount to the existing account.
-    /// # Errors
-    /// Attempted overflow
-    pub fn deposit(&mut self, signer: &str, amount: u64) -> Result<tx::Tx, error::AccountingError> {
-        if let Some(account) = self.accounts.get_mut(signer) {
-            (*account)
-                .checked_add(amount)
-                .and_then(|r| {
-                    *account = r;
-                    Some(r)
-                })
-                .ok_or(error::AccountingError::AccountOverFunded(
-                    signer.to_string(),
-                    amount,
-                ))
-                // Using map() here is an easy way to only manipulate the non-error result
-                .map(|_| tx::Tx::Deposit {
+    impl Accounts {
+        /// Returns an empty instance of the [`Accounts`] type
+        pub fn new() -> Self {
+            Accounts {
+                accounts: Default::default(),
+            }
+        }
+
+        /// Either deposits the `amount` provided into the `signer` account or adds the amount to the existing account.
+        /// # Errors
+        /// Attempted overflow
+        pub fn deposit(&mut self, signer: &str, amount: u64) -> Result<tx::Tx, error::AccountingError> {
+            if let Some(account) = self.accounts.get_mut(signer) {
+                (*account)
+                    .checked_add(amount)
+                    .and_then(|r| {
+                        *account = r;
+                        Some(r)
+                    })
+                    .ok_or(error::AccountingError::AccountOverFunded(
+                        signer.to_string(),
+                        amount,
+                    ))
+                    // Using map() here is an easy way to only manipulate the non-error result
+                    .map(|_| tx::Tx::Deposit {
+                        account: signer.to_string(),
+                        amount,
+                    })
+            } else {
+                self.accounts.insert(signer.to_string(), amount);
+                Ok(tx::Tx::Deposit {
                     account: signer.to_string(),
                     amount,
                 })
-        } else {
-            self.accounts.insert(signer.to_string(), amount);
-            Ok(tx::Tx::Deposit {
-                account: signer.to_string(),
-                amount,
-            })
+            }
         }
-    }
 
-    /// Withdraws the `amount` from the `signer` account.
-    /// # Errors
-    /// Attempted overflow
-    pub fn withdraw(&mut self, signer: &str, amount: u64) -> Result<tx::Tx, error::AccountingError> {
-        if let Some(account) = self.accounts.get_mut(signer) {
-            account
-                .checked_sub(amount)
-                .and_then(|r| {
-                    *account = r;
-                    Some(r)
-                })
-                .ok_or(error::AccountingError::AccountUnderFunded(signer.to_string(), amount))
-                .map(|_| tx::Tx::Withdraw {
-                    account: signer.to_string(),
+        /// Withdraws the `amount` from the `signer` account.
+        /// # Errors
+        /// Attempted overflow
+        pub fn withdraw(&mut self, signer: &str, amount: u64) -> Result<tx::Tx, error::AccountingError> {
+            if let Some(account) = self.accounts.get_mut(signer) {
+                account
+                    .checked_sub(amount)
+                    .and_then(|r| {
+                        *account = r;
+                        Some(r)
+                    })
+                    .ok_or(error::AccountingError::AccountUnderFunded(signer.to_string(), amount))
+                    .map(|_| tx::Tx::Withdraw {
+                        account: signer.to_string(),
+                        amount,
+                    })
+            } else {
+                Err(error::AccountingError::AccountNotFound(signer.to_string()))
+            }
+        }
+
+        /// Withdraws the amount from the sender account and deposits it in the recipient account.
+        ///
+        /// # Errors
+        /// The account doesn't exist
+        pub fn send(
+            &mut self,
+            sender: &str,
+            recipient: &str,
+            amount: u64,
+        ) -> Result<(tx::Tx, tx::Tx), error::AccountingError> {
+            let Some(_) = self.accounts.get_mut(sender) else {
+                return Err(error::AccountingError::AccountNotFound(sender.to_string()));
+            };
+            let Some(_) = self.accounts.get_mut(recipient) else {
+                return Err(error::AccountingError::AccountNotFound(recipient.to_string()));
+            };
+            let Ok(withdraw) = self.withdraw(sender, amount) else {
+                return Err(error::AccountingError::AccountUnderFunded(
+                    sender.to_string(),
                     amount,
-                })
-        } else {
-            Err(error::AccountingError::AccountNotFound(signer.to_string()))
+                ));
+            };
+            let Ok(deposit) = self.deposit(recipient, amount) else {
+                // return the amount to sender
+                self.deposit(sender, amount)?;
+                return Err(error::AccountingError::AccountOverFunded(
+                    recipient.to_string(),
+                    amount,
+                ));
+            };
+            Ok((withdraw, deposit))
         }
-    }
-
-    /// Withdraws the amount from the sender account and deposits it in the recipient account.
-    ///
-    /// # Errors
-    /// The account doesn't exist
-    pub fn send(
-        &mut self,
-        sender: &str,
-        recipient: &str,
-        amount: u64,
-    ) -> Result<(tx::Tx, tx::Tx), error::AccountingError> {
-        let Some(_) = self.accounts.get_mut(sender) else {
-            return Err(error::AccountingError::AccountNotFound(sender.to_string()));
-        };
-        let Some(_) = self.accounts.get_mut(recipient) else {
-            return Err(error::AccountingError::AccountNotFound(recipient.to_string()));
-        };
-        let Ok(withdraw) = self.withdraw(sender, amount) else {
-            return Err(error::AccountingError::AccountUnderFunded(
-                sender.to_string(),
-                amount,
-            ));
-        };
-        let Ok(deposit) = self.deposit(recipient, amount) else {
-            // return the amount to sender
-            self.deposit(sender, amount)?;
-            return Err(error::AccountingError::AccountOverFunded(
-                recipient.to_string(),
-                amount,
-            ));
-        };
-        Ok((withdraw, deposit))
     }
 }
+
+
 
 fn read_from_stdin(label: &str) -> String {
     print!("{}", label);
@@ -132,7 +137,7 @@ fn read_from_stdin(label: &str) -> String {
 }
 
 fn main() {
-    let mut ledger = Accounts::new();
+    let mut ledger = accounts::Accounts::new();
     let mut tx_log = vec![];
     loop {
         let line = read_from_stdin("cmd: ");
@@ -161,7 +166,7 @@ fn main() {
     }
 }
 
-fn cmd_send(ledger: &mut Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, from: &&str, to: &&str) {
+fn cmd_send(ledger: &mut accounts::Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, from: &&str, to: &&str) {
     if let Ok(amount) = amount.parse::<u64>() {
         match ledger.send(from, to, amount) {
             Ok((Tx1, Tx2)) => tx_log.append(vec![Tx1, Tx2].as_mut()),
@@ -174,7 +179,7 @@ fn cmd_send(ledger: &mut Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, from
     };
 }
 
-fn cmd_deposit(ledger: &mut Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, signer: &&str) {
+fn cmd_deposit(ledger: &mut accounts::Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, signer: &&str) {
     if let Ok(amount) = amount.parse::<u64>() {
         match ledger.deposit(signer, amount) {
             Ok(Tx) => tx_log.push(Tx),
@@ -187,7 +192,7 @@ fn cmd_deposit(ledger: &mut Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, s
     };
 }
 
-fn cmd_withdraw(ledger: &mut Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, signer: &&str) {
+fn cmd_withdraw(ledger: &mut accounts::Accounts, tx_log: &mut Vec<tx::Tx>, amount: &&str, signer: &&str) {
     if let Ok(amount) = amount.parse::<u64>() {
         match ledger.withdraw(signer, amount) {
             Ok(tx) => tx_log.push(tx),
